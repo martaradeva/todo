f68b320cb7d50490297203ba85e225ba6b4295b0
Martina Radeva <martaradeva@yahoo.com>
2015-02-19 18:16:07 +0200

    Database migrated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150112131435) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
